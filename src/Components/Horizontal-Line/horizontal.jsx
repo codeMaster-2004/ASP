16b6a0046aeec2f4b4bee2161de7b3faa4cc53b7
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-20 21:22:53 -0500

    horizontal and vertical lines added separately

---
+++ src/Components/Horizontal-Line/horizontal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Horizontal = ({ height }) => {
+  return (
+    <div
+      style={{
+        height: `${height}px`,
+        backgroundColor: '#000',
+        margin: '0 11.5%'
+        // Add any other styles or content you want to include
+      }}
+    />
+  );
+};
+
+export default Horizontal;
---
+++ src/Components/Vertical/verticalLine.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Vertical = ({ width }) => {
+  return (
+    <div
+      style={{
+        width: `${height}px`,
+        height: '100%',
+        backgroundColor: '#000',
+        margin: '0 11.5%'
+        // Add any other styles or content you want to include
+      }}
+    />
+  );
+};
+
+export default Vertical;
