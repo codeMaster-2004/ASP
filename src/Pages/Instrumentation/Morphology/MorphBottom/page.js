2649b3150a9f84f87074449a4a9f57ac6ec4020f
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-22 17:00:11 -0500

    updated font styles

---
+++ src/Pages/Instrumentation/Morphology/MorphBottom/page.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Separator from '../../../../Components/Orange-separator/separator';
+import './page.css';
+
+function MorphBottom() {
+  return (
+    <div className='morph-bottom-conatiner'>
+        <div className="morph-bottom-agglomerated-effects">
+            <h1>
+                Effects of Sonication on Breaking Agglomerated Nanoparticles
+            </h1>
+            <p>
+                We suspended each of these powders separately in water and sonicated the suspension to disperse the particles. The extinction spectra of these suspensions obtained using an off-the-shelf spectrometer are shown in the figure below.
+            </p>
+            <p>
+                Addition of surfactants and high intensity sonication are commonly used to break up agglomerated particles in various applications like chemical mechanical planarization (CMP). In order to examine this further, we measured the morphology of CMP slurry samples subjected to sonication. Sample results for silica CMP slurries are presented in the figure below. 
+            </p>
+        </div>
+        <Separator />
+        <div className='morph-bottom-agglomerated-effects-image'>
+            <img src={`${process.env.PUBLIC_URL}/images/agloeffects.png`} alt='effects' width='100%' height='100%' />
+            <p>
+                Effect of sonication on breaking up the agglomerates to improve dispersion of silica nanoparticles.
+            </p>
+        </div>
+        <div className='morph-bottom-agglomerated-text'>
+            <p>
+                Here we present the equivalent spherical particle size distribution for simplicity. The virgin sample (without sonication) exhibited a particle size range of 80 to 300 nm with a mode of about 180 nm. 8 minutes of sonication reduced the particle size range to 50 to 280 nm with a bimodal distribution with modes of about 60 nm and 160 nm, while 15 minutes of sonication changed the distribution to trimodal with modes of 75 nm, 120 nm, and 225 nm. 
+            </p>
+            <p>
+                A closer examination of the three distributions suggests that an increase in sonication duration increases the proportion of smaller particles in the sample. This clearly shows that the nanoparticles do undergo agglomeration in silica CMP slurries and that sonication is an effective strategy to break up the agglomerates. 
+            </p>
+            <p>
+                We performed a similar study on alumina CMP slurries. We measured the fractal dimension of the particles in the suspension after subjecting to sonication for different periods of time. The virgin sample (with no sonication) yielded a fractal dimension of 2.63. Intense sonication for 1 minute lowered the fractal dimension significantly to about 1.92. Additional sonication led to fractal dimensions in the range of 1.5 to 1.81. 
+            </p>
+        </div>
+        <Separator />
+        <div className='CMP-effects'>
+            <img src={`${process.env.PUBLIC_URL}/images/agloeffects.png`} alt='effects' width='100%' height='100%' />
+        </div>
+    </div>
+  )
+}
+
+export default MorphBottom
--- src/Pages/Instrumentation/Morphology/main.js
+++ src/Pages/Instrumentation/Morphology/main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import MorphTop from './MorphTop/page';
+import './main.css';
+import MorphBottom from './MorphBottom/page';
 
 function Morphology() {
   return (
     <div className='morphology'>
         <MorphTop />
+        <MorphBottom />
     </div>
   )
 }
