2dd872558394c39e65432c6398cd3daf4d0d0369
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-23 10:55:04 -0500

    added 2 contact folders

---
+++ src/Pages/ContactUs/ContactTop/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactTop() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactTop
--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -5,15 +5,6 @@ import { Button } from '../../Components/Button/Button';
 function ContactUs () {
   return (
     <div className="contactUs">
-      <div className='contactUs-container'>
-        <p>
-          Home
-        </p>
-        <h1>
-          Ready to put yourself in expert hands?
-        </h1>
-
-      </div>
     </div>
   )
 }
