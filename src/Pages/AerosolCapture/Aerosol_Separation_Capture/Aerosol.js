3f5837a129de47dfd1547ae8393924d86a6ed885
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-23 02:17:52 -0500

    about to edit the aerosol capture

--- src/Pages/AerosolCapture/Aerosol_Separation_Capture/Aerosol.js
+++ src/Pages/AerosolCapture/Aerosol_Separation_Capture/Aerosol.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Aerosol.css';
 import AerosolIntro from './Aerosol_Intro/page';
 import Innovation from './Innovation/page';
 
 function Aerosol() {
   return (
-    <div>
+    <div className='SepAndCapture'>
         <AerosolIntro />
         <Innovation />
     </div>
