6ac72e7a7baccb28b313d7f3a3a59198ff467963
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-23 10:50:02 -0500

    deleted contact us folders

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import ContactInfo from './Bottom-info/page';
+import React from 'react';
+import './ContactUs.css';
+import { Button } from '../../Components/Button/Button';
 
-function ContactUs() {
+function ContactUs () {
   return (
-    <>
-      <ContactInfo/>
-    </>
+    <div className="contactUs">
+      <div className='contactUs-container'>
+        <p>
+          Home
+        </p>
+        <h1>
+          Ready to put yourself in expert hands?
+        </h1>
+
+      </div>
+    </div>
   )
 }
-
 export default ContactUs
