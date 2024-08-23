c50f32a0452e98b8b975a43848c039336ff37886
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-23 11:02:47 -0500

    done with contact top

--- src/Pages/ContactUs/ContactTop/page.js
+++ src/Pages/ContactUs/ContactTop/page.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './page.css';
 
 function ContactTop() {
   return (
-    <div>
-      
-    </div>
+    <div className='contactUs-container'>
+        <p>
+          Home
+        </p>
+        <h1>
+          Ready to put yourself in expert hands?
+        </h1>
+      </div>
   )
 }
 
--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ContactUs.css';
 import { Button } from '../../Components/Button/Button';
+import ContactTop from './ContactTop/page';
 
 function ContactUs () {
   return (
     <div className="contactUs">
+      <ContactTop />
     </div>
   )
 }
