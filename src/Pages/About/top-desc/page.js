e1c340cbcde89b54622e9802f059ebe43fc3d277
Nee-ayya <Kundan.mergu@gmail.com>
2024-08-27 00:12:36 -0500

    About us Page Fixed

--- src/Pages/About/top-desc/page.js
+++ src/Pages/About/top-desc/page.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './page.css';
 
 function page() {
   return (
     <>
       <div className='top'>
-          <div className='huge-text'>
-              <h1>About Us</h1>
-          </div>
-      </div>
-      <div className='earth'>
-        <div className='earth-text'>
-          <div className='sep1'/>
-          <div className='text'>
-            <p>
-              “Our vision is to translate our deep knowledge and combined experience into commercially viable products and technologies that make the world a safer place.” 
-            </p>
-          </div>
-          <div className='sep2'/>
+        <div className='top-left'>
+          <h1>
+            “Our vision is to <span className='highlight'>translate</span> our deep <span className='highlight'>knowledge</span> and combined <span className='highlight'>experience</span> into commercially viable <span className='highlight'>products and technologies</span> that make the world a safer place.” 
+          </h1>
         </div>
-      </div>
-      <div className='black-background'>
-        <div className='black-text'>
-          <p>
-            Led by four senior staff members, our exceptionally talented team excels in innovation. With vast domain expertise, we develop simple yet cutting-edge solutions to the complex challenges facing our industry.
-          </p>
-        </div>
+        
       </div>
       <div className='exp'>
         <div className='exp-container'>
