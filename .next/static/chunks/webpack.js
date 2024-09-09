7e21bb8bfe3bb2effc0f2fa7070d07986d099ee3
Nee-ayya <Kundan.mergu@gmail.com>
2024-09-09 17:48:12 -0500

    edited classnames

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18403694c0926f2f"; }
+/******/ 		__webpack_require__.h = function() { return "821300ab10994313"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/Pages/Homepage/Cards/Cards.js
+++ src/Pages/Homepage/Cards/Cards.js
@@ -1,42 +1,42 @@
 import React from 'react';
-import './Cards.module.css'; 
+import styles from './Cards.module.css'; 
 import CustomLink from '../../../Components/CustomLink/CustomeAtt';
 // width='576.8vw' height="400em"
 // width='540vw' height="400em"
 function Cards() {
   return (
-    <div className='cards'>
-      <div className='cards__container'>
-          <div className='cards__container_left'>
-            <img src={`${process.env.PUBLIC_URL}/images/image-27.png`} alt='products main' width='576.8vw' height="400em"/>
-            <div className='cards__container_left_info'>
-              <div className='cards__container_left_info_conatainer'>
-                <div className='cards__container_left_info_heading'>
+    <div className={styles.cards}>
+      <div className={styles.cards__container}>
+          <div className={styles.cards__container_left}>
+            <img src={`/images/image-27.png`} alt='products main' width='576.8vw' height="400em"/>
+            <div className={styles.cards__container_left_info}>
+              <div className={styles.cards__container_left_info_conatainer}>
+                <div className={styles.cards__container_left_info_heading}>
                   <h1>Our Facilities</h1>
-                  <div className='cards_separator1'/>
+                  <div className={styles.cards_separator1}/>
                 </div>
                 <p>
                   Our laboratory is equipped with unique in-house constructed and third-party hardware and instrumentation to characterize the hazards and validate the effectiveness of safety technologies.
                 </p>
               </div>
-              <CustomLink to='/facilities' className='cards_button'>
+              <CustomLink to='/facilities' className={styles.cards_button}>
                 Learn more
               </CustomLink>
             </div>
           </div>
-          <div className='cards__container_right'>
-            <img src={`${process.env.PUBLIC_URL}/images/HomeCards.png`} alt='products main' width='540vw' height="400em"/>
-            <div className='cards__container_right_info'>
-              <div className='cards__container_right_info_conatainer'>
-                <div className='cards__container_right_info_heading'>
+          <div className={styles.cards__container_right}>
+            <img src={`/images/HomeCards.png`} alt='products main' width='540vw' height="400em"/>
+            <div className={styles.cards__container_right_info}>
+              <div className={styles.cards__container_right_info_conatainer}>
+                <div className={styles.cards__container_right_info_heading}>
                   <h1>Our Products and Services</h1>
-                  <div className='cards_separator'/>
+                  <div className={styles.cards_separator}/>
                 </div>
                 <p>
                   We provide engineering and computational analysis, specialized testing and custom instrumentation to help improve the performance and safety of your products and processes. 
                 </p>
               </div>
-              <CustomLink to='/products-services' className='cards_button'>
+              <CustomLink to='/products-services' className={styles.cards_button}>
                 Learn more
               </CustomLink>
             </div>
