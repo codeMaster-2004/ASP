bd2b5a858249a915c317c7e85bcd6f06329f8b21
Nee-ayya <Kundan.mergu@gmail.com>
2024-09-12 05:23:23 -0500

    edited a few things

--- next-config.js
+++ next-config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: '/ASP/',
+  basePath: '/ASP',
   images: {
     unoptimized: true,
   },
 }
 
 module.exports = nextConfig
-basePath: '/ASP'
