e7b37608a1a6cd7c300f0da8ddcf832014606ef5
Kundan Mergu <116580017+codeMaster-2004@users.noreply.github.com>
2024-11-05 11:10:28 -0600

    Update next.config.js to include assetPrefix AND trailing slash

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ASP',
-  trailingSlash: true,
+  basePath: '/ASP',  // GitHub Pages will serve the site under /ASP
+  assetPrefix: './',  // Set to relative so paths adjust depending on the domain
+  trailingSlash: true,  // Ensures URLs end with a slash for consistency
   images: {
     unoptimized: true,
     loader: 'custom',
     loaderFile: './imageLoader.js',
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
