22c8f7c0cf7e3832607b845b01404e8743003076
Kundan Mergu <116580017+codeMaster-2004@users.noreply.github.com>
2024-11-06 12:16:53 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,13 @@
 // next.config.js
 const nextConfig = {
   output: 'export',
-  assetPrefix: '',
-  basePath: '',
   images: {
     unoptimized: true,
-    loader: 'custom',
-    loaderFile: './imageLoader.js',
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
 module.exports = nextConfig;
+
