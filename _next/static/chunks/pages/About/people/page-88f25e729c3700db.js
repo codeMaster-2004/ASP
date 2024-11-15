c920463500e1da5f4134ea7583297ff0d1f65dea
Nee-ayya <Kundan.mergu@gmail.com>
2024-11-15 14:35:17 -0600

    Updates

---
+++ _next/static/chunks/pages/About/people/page-88f25e729c3700db.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[979],{9509:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/About/people/page",function(){return u(1062)}])}},function(n){n.O(0,[62,888,774,179],function(){return n(n.s=9509)}),_N_E=n.O()}]);
--- _next/static/chunks/pages/Instrumentation/Morphology/main-61ebbf14642da8d5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[500],{127:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Instrumentation/Morphology/main",function(){return u(6198)}])}},function(n){n.O(0,[644,655,198,888,774,179],function(){return n(n.s=127)}),_N_E=n.O()}]);
