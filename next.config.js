190cbd292cef1bf3b1a03192fe7456979088e7ec
Nee-ayya <Kundan.mergu@gmail.com>
2024-11-18 04:00:11 -0600

    edited cname

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const nextConfig = {
-  assetPrefix: '/ASP',
-  basePath: '/ASP', 
-  images: {
-    unoptimized: true,
-    loader: 'custom',
-    loaderFile: './imageLoader.js',
-  },
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
 // const nextConfig = {
-//   output: 'export',
-//   assetPrefix: process.env.NODE_ENV === 'production' ? '/ASP' : '',
-//   basePath: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+//   assetPrefix: '/ASP',
+//   basePath: '', 
 //   images: {
 //     unoptimized: true,
+//     loader: 'custom',
+//     loaderFile: './imageLoader.js',
 //   },
 //   trailingSlash: true,
 // }
 
 // module.exports = nextConfig
+const nextConfig = {
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
