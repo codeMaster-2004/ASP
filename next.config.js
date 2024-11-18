23f924cad9a9251a43346c44452993e28c1250e7
Nee-ayya <Kundan.mergu@gmail.com>
2024-11-18 03:46:52 -0600

    back to original config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
-// const nextConfig = {
-//   assetPrefix: '/ASP',
-//   basePath: '', 
-//   images: {
-//     unoptimized: true,
-//     loader: 'custom',
-//     loaderFile: './imageLoader.js',
-//   },
-// }
 const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ASP' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+  assetPrefix: '/ASP',
+  basePath: '/ASP', 
   images: {
     unoptimized: true,
+    loader: 'custom',
+    loaderFile: './imageLoader.js',
   },
   trailingSlash: true,
 }
 
 module.exports = nextConfig
+// const nextConfig = {
+//   output: 'export',
+//   assetPrefix: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+//   basePath: process.env.NODE_ENV === 'production' ? '/ASP' : '',
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+// }
+
+// module.exports = nextConfig
